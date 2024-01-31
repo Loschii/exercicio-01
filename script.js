ddef2ca90daf5ec5366a53458517b13df9e5d2a4
Eduardo Lima Boucault <107961223+eduardoboucault@users.noreply.github.com>
2024-01-31 10:23:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// console.log('Exercício 01')
+
+// const input01 = prompt('Olá, qual é o seu nome?')
+
+// console.log(`Olá ${input01}, tudo bem?`)
+
+// Para realizar os prompts dentro do seu site local é necessário ter a extensão Live Server instala no vscode.
+
+// Crie seu arquivo index.html
+
+// Crie seu arquivo script.js
+
+// Crie seu arquivo style.css
+
+// Faça a configuração de linkar os arquivos script e style no index e inicie o live server!
+
+// Depois descomente as linhas 01 03 05 deste código e atualize novamente sua página.
